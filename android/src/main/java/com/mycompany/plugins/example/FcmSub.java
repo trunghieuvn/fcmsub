ed6255488bb90038167debd26ac232550c09c063
HieuTranTrung <hieu.trantrung1204@gmail.com>
2023-02-06 03:01:11 +0700

    Add FirebaseMessaging

--- android/src/main/java/com/mycompany/plugins/example/FcmSub.java
+++ android/src/main/java/com/mycompany/plugins/example/FcmSub.java
@@ -1,11 +1,13 @@
 package com.mycompany.plugins.example;
 
 import android.util.Log;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class FcmSub {
 
     public String echo(String value) {
         Log.i("Echo", value);
+        FirebaseMessaging.getInstance().subscribeToTopic("public");
         return value;
     }
 }
